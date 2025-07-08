4562422e052fd87d64507bb0a0766c32e0e96dcd
atticus <144712135+atile4@users.noreply.github.com>
2025-07-08 16:15:02 -0700

    Update main.py

--- comparer/main.py
+++ comparer/main.py
@@ -1,5 +1,5 @@
-from scrape import makeTableList, printCourse
 from majorchecker import majorCheck, MAJORS_LIST
+from compare import comparer
 
 def main():
     print("----------------------------------------\n"
@@ -7,58 +7,27 @@ def main():
       "|                                      |\n"
       "|           written by atile           |\n"
       "----------------------------------------\n")
-    print("Welcome to UCI Major Comparison!\n\nType help to get started.")
+    print("Welcome to UCI Major Comparison!\n\n"
+          "Type help to get started.")
 
     run = True
     while run:
         cmd : str = input("> ")
         print()
-        if (cmd == "help"):
-            print("\n========COMMANDS========\n"
+        
+        match cmd:
+            case "compare":
+                comparer()
+            case "help":
+               print("\n========COMMANDS========\n"
                     "help: Displays this page\n"
                     "exit: terminates the program\n"
                     "majors: list of majors\n"
                     "compare: input 2 majors to compare\n"+
-                    ("=")*25)
-            
-        elif (cmd == "compare"):
-            response1 = input("Input the first major, as listed from the UCI catalogue.\n> ")
-            website1 = majorCheck(response1)
-            table_list1, courselist1 = makeTableList(website1)
-            
-            response2 = input("Input the first major, as listed from the UCI catalogue.\n> ")
-            website2 = majorCheck(response2)
-            table_list2, courselist2 = makeTableList(website=website2)
-            
-            overlap = list(set(courselist1) & set(courselist2))
-            overlap_length = len(overlap)
-            newcourse1 = [x for x in table_list1 if x in overlap or " : " not in x]
-            newcourse2 = [x for x in table_list2 if x in overlap or " : " not in x]
-            print("\nNumber of courses that overlap between " + response1.capitalize() + " and " + response2.capitalize() + ":", overlap_length)
-            
-            print(response1.upper())
-            printCourse(newcourse1)
-
-            print(response2.upper())
-            printCourse(newcourse2)
-            
-            print("For", response1.capitalize() + ",", str(overlap_length) + "/" + str(len(set(courselist1))) + " have overlap.")
-            print("For", response2.capitalize() + ",", str(overlap_length) + "/" + str(len(set(courselist2))) + " have overlap.")
-
-
-        elif (cmd == "majors"):
-            print(MAJORS_LIST)
-        elif (cmd == "exit" or cmd == "quit"):
-            print("Thanks for using.")
-            run = False
-
-        else:
-            print("Invalid command. Type help for a list of commands.")
-
-
+                    "=========================") 
+            case "majors": print(MAJORS_LIST)
+            case "exit" | "quit":
+                print("Thanks for using.")
+                run = False
+            case _: print("Invalid command. Type help for a list of commands.")
 main()
-    
-
-
-
-    
