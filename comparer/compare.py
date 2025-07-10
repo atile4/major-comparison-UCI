005cae5e74f710d935049472f4532c6745848c45
atticus <144712135+atile4@users.noreply.github.com>
2025-07-09 22:41:25 -0700

    Update compare.py
    Fixed an issue where, when inputting a major, the program would save the first major input instead of the last input

--- comparer/compare.py
+++ comparer/compare.py
@@ -1,13 +1,24 @@
 from scrape import makeTableList, printCourse
-from majorchecker import majorCheck, MAJORS_LIST
+from majorchecker import isMajor, MAJORS
     
 def comparer():
-    response1 = input("Input the first major, as listed from the UCI catalogue.\n> ")        
+    response1 = input("Input the first major, as listed from the UCI catalogue.\n> ")
+    while not isMajor(response1):
+        if (response1 == "exit"):
+            exit()
+        else:
+            response1 = input("Invalid major. Please input a major as listed from the UCI catalogue.\n> ")
+    
     response2 = input("Input the second major, as listed from the UCI catalogue.\n> ")
+    while not isMajor(response2):
+        if (response2 == "exit"): exit()
+        else:
+            response2 = input("Invalid major. Please input a major as listed from the UCI catalogue.\n> ")
+            
+    website1 = MAJORS[response1]
+    website2 = MAJORS[response2]
 
-    website1 = majorCheck(response1)
-    table_list1, courselist1 = makeTableList(website1)
-    website2 = majorCheck(response2)
+    table_list1, courselist1 = makeTableList(website=website1)  
     table_list2, courselist2 = makeTableList(website=website2)
     
     overlap = list(set(courselist1) & set(courselist2))
@@ -15,7 +26,7 @@ def comparer():
     newcourse1 = [x for x in table_list1 if x in overlap or " : " not in x]
     newcourse2 = [x for x in table_list2 if x in overlap or " : " not in x]
                 
-    print(response1.upper())
+    print("\n"+response1.upper())
     printCourse(newcourse1)
 
     print("\n"+response2.upper())
@@ -26,3 +37,4 @@ def comparer():
     print("For", response2.capitalize() + ",", str(overlap_length) + "/" + str(len(set(courselist2))) + " courses have overlap.")
     
     print("\nNumber of courses that overlap between " + response1.capitalize() + " and " + response2.capitalize() + ":", overlap_length)
+    print("\nInput your next command.")
